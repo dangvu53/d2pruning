d42aa5cbe7dee9af6d5ed7776ad87882c5e7bb6a
haidang241 <23020042@vnu.edu.vn>
2025-06-29 11:43:45 +0700

    feat: add features map for efficientnet, mobilenet

--- core/model_generator/models/efficientnet.py
+++ core/model_generator/models/efficientnet.py
@@ -139,6 +139,14 @@ class EfficientNet(nn.Module):
                 in_channels = out_channels
         return nn.Sequential(*layers)
 
+    def feature_map(self, x):
+        out = swish(self.bn1(self.conv1(x)))
+        out = self.layers(out)
+        out = F.adaptive_avg_pool2d(out, 1)
+        out = out.view(out.size(0), -1)
+        return out
+
+
     def forward(self, x):
         out = swish(self.bn1(self.conv1(x)))
         out = self.layers(out)
